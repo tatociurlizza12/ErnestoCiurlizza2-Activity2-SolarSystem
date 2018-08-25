724b1a8ec349eaeaf6b10f07af6cb1335e9a6541
tatociurlizza12 <tciurlizzas@gmail.com>
2018-08-25 13:28:48 -0500

    Segunda version

---
+++ SolarSystem/Assets/Scripts/Translation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Translation : MonoBehaviour {
+
+    public Transform rotationPivot;
+    public float translationSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.RotateAround(rotationPivot.position, Vector3.up, translationSpeed * Time.deltaTime);
+	}
+}
